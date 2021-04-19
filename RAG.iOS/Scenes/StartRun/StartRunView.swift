6566a0638e110e7d1ece45751450b5908e4972af
Bjorn Derudder <bjorn.derudder@gmail.com>
2021-04-19 18:48:16 +0200

    added the very beginning of the tracking

--- RAG.iOS/Scenes/StartRun/StartRunView.swift
+++ RAG.iOS/Scenes/StartRun/StartRunView.swift
@@ -6,10 +6,31 @@
 //
 
 import SwiftUI
+import CoreLocation
 
 struct StartRunView: View {
+    let locationManager = CLLocationManager()
+    let delegate = LocationDelegate()
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .onAppear(perform: {
+                self.locationManager.requestAlwaysAuthorization()
+                print(CLLocationManager.locationServicesEnabled())
+                if CLLocationManager.locationServicesEnabled() {
+                    print("YES")
+                    locationManager.delegate = delegate
+                    locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+                    locationManager.startUpdatingLocation()
+                }
+            })
+    }
+}
+
+class LocationDelegate: NSObject, CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return }
+        print("locations = \(locValue.latitude) \(locValue.longitude)")
     }
 }
 
