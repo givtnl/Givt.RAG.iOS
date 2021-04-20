133e802db5de8952dc9dcd852472ea8d0a69702d
Mike Pattyn <mike@givtapp.net>
2021-04-20 17:09:44 +0200

    add a map to tracking view

--- RAG.iOS/Scenes/Tracking/TrackingView.swift
+++ RAG.iOS/Scenes/Tracking/TrackingView.swift
@@ -6,56 +6,81 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct TrackingView: View {
     @ObservedObject var runningManager = RunningService();
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(
+            latitude: 25.7617,
+            longitude: 80.1918
+        ),
+        span: MKCoordinateSpan(
+            latitudeDelta: 10,
+            longitudeDelta: 10
+        )
+    )
+    
     var body: some View {
-        VStack {
+        ZStack {
+            Map(coordinateRegion: $region)
             VStack {
-                Text(String(format: "%.1f", runningManager.currentTime))
-                    .font(Font.custom("Montserrat-Bold", size: 50))
-                HStack {
+                Spacer()
+                VStack {
                     VStack {
-                        Text(String(format: "%.2f", runningManager.currentDistance/1000))
-                            .font(Font.custom("Montserrat-Bold", size: 25))
-                        Text("km")
-                            .font(Font.custom("Montserrat-Medium", size: 12))
+                        Text(String(format: "%.1f", runningManager.currentTime))
+                            .font(Font.custom("Montserrat-Bold", size: 50))
+                        HStack {
+                            VStack {
+                                Text(String(format: "%.2f", runningManager.currentDistance/1000))
+                                    .font(Font.custom("Montserrat-Bold", size: 25))
+                                Text("km")
+                                    .font(Font.custom("Montserrat-Medium", size: 12))
+                            }
+                            VStack {
+                                Text(String(format: "%i:%02d", runningManager.currentPace.0, runningManager.currentPace.1))
+                                    .font(Font.custom("Montserrat-Bold", size: 25))
+                                Text("min/km")
+                                    .font(Font.custom("Montserrat-Medium", size: 12))
+                            }
+                        }
+                        .padding(.top, 15)
                     }
-                    VStack {
-                        Text(String(format: "%i:%02d", runningManager.currentPace.0, runningManager.currentPace.1))
-                            .font(Font.custom("Montserrat-Bold", size: 25))
-                        Text("min/km")
-                            .font(Font.custom("Montserrat-Medium", size: 12))
+                    .frame(
+                        minWidth: 0,
+                        maxWidth: .infinity,
+                        alignment: .center
+                    )
+                    .padding(40)
+                    
+                    Button(runningManager.state == RunningSystemState.Running ? "Stop" : "Start") {
+                        if (runningManager.state == RunningSystemState.Running) {
+                            runningManager.stopRunning()
+                            runningManager.initSystems()
+                        } else {
+                            runningManager.startRunning()
+                        }
                     }
-                }.padding(.top, 15)
-            }.frame(
+                    .font(Font.custom("Montserrat-SemiBold", size: 14))
+                    .frame(width: 240, height: 50).background(Color.black)
+                    .foregroundColor(.white)
+                    .cornerRadius(25)
+                    .disabled(runningManager.state == .Initializing || runningManager.state == .Stopped)
+                    .padding(.bottom, 20)
+                    
+                }.background(Color.white).cornerRadius(25, corners: .topRight)
+            }
+            .onAppear(perform: {
+                runningManager.initSystems()
+            })
+            .frame(
                 minWidth: 0,
                 maxWidth: .infinity,
-                alignment: .center
-            )
-            .padding(40)
-            .background(Color("DarkerPink"))
-            .cornerRadius(10)
-            
-            Button(runningManager.state == RunningSystemState.Running ? "Stop" : "Start") {
-                if (runningManager.state == RunningSystemState.Running) {
-                    runningManager.stopRunning()
-                    runningManager.initSystems()
-                } else {
-                    runningManager.startRunning()
-                }
-            }
-            .font(Font.custom("Montserrat-SemiBold", size: 14))
-            .frame(width: 100, height: 35).background(Color.black)
-            .foregroundColor(.white)
-            .cornerRadius(10)
-            .offset(y: 30)
-            .disabled(runningManager.state == .Initializing || runningManager.state == .Stopped)
-        }
-        .padding(30)
-        .onAppear(perform: {
-            runningManager.initSystems()
-        })
+                minHeight: 0,
+                maxHeight: .infinity,
+                alignment: .topLeading
+            ).ignoresSafeArea(edges: .top)
+        }.ignoresSafeArea(edges: .top)
     }
 }
 
