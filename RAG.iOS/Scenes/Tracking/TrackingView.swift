115853f98103db7e68ca6157aeba968f7076bf17
Mike Pattyn <mike@givtapp.net>
2021-04-20 11:01:16 +0200

    add view for tracking

---
+++ RAG.iOS/Scenes/Tracking/TrackingView.swift
@@ -0,0 +1,20 @@
+//
+//  TrackingView.swift
+//  RAG.iOS
+//
+//  Created by Mike Pattyn on 20/04/2021.
+//
+
+import SwiftUI
+
+struct TrackingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TrackingView_Previews: PreviewProvider {
+    static var previews: some View {
+        TrackingView()
+    }
+}
