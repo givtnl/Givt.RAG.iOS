1918035dd3e0e791ebca46798ca2dedb68ae3cf1
Mike Pattyn <mike@givtapp.net>
2021-04-20 09:55:57 +0200

    remove not using code

--- RAG.iOS/Scenes/Event/EventView.swift
+++ RAG.iOS/Scenes/Event/EventView.swift
@@ -40,17 +40,10 @@ struct EventView: View {
                             .font(.caption)
                             .fontWeight(.light)
                             .padding(.top, 1)
-                        Text("Lees meer")
-                            .font(.caption)
-                            .fontWeight(.bold)
-                            .foregroundColor(.red)
-                            .padding(.top, 1)
                         NavigationLink(destination: StartRunView()) {
                             Text("Start running")
                         }
                     }.offset(y: 15)
-                    
-                    
                 }.frame(
                     minWidth: 0,
                     maxWidth: .infinity,
@@ -65,54 +58,10 @@ struct EventView: View {
                 maxHeight: .infinity,
                 alignment: .topLeading
             ).ignoresSafeArea(edges: .all)
-            .onAppear(perform: {
-                viewAppeared()
-            })
         }
 
-    }
-    
-    private func viewAppeared() {
-    }
-    
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-            
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-    
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-            
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
     }
 }
-
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
