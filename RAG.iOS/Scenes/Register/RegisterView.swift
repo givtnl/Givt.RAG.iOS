67d599e0a752b33c9df37a74785d3135a8b20d2c
Mike Pattyn <mike@givtapp.net>
2021-04-20 11:31:09 +0200

    added fonts and color to assets

--- RAG.iOS/Scenes/Register/RegisterView.swift
+++ RAG.iOS/Scenes/Register/RegisterView.swift
@@ -12,12 +12,49 @@ struct RegisterView: View {
 
     var body: some View {
         VStack {
-            TextField("Username", text: $modelData.userName)
-            TextField("Email", text: $modelData.email)
-            NavigationLink(destination: ProfileView(profile: modelData)) {
-                Text("Save")
-            }
-        }
+            HStack {
+                VStack {
+                    Text("07")
+                    Text("Aug")
+                }.padding(10).background(Color("LightPink"))
+                VStack {
+                    Text("Sponsor run")
+                    Text("Kortrijk, 01:00 PM - 04:00 PM")
+                }
+            }.padding(10).background(Color.white).cornerRadius(5, corners: .allCorners).shadow(radius: 5)
+            VStack(alignment: .leading) {
+                Text("I want to run!")
+                
+                TextField("Username", text: $modelData.userName)
+                    .padding(.leading, 10)
+                    .frame(height: 40)
+                    .background(Color("DarkerPink"))
+                    .cornerRadius(5)
+                    
+                TextField("Email", text: $modelData.email)
+                    .padding(.leading, 10)
+                    .frame(height: 40)
+                    .background(Color("DarkerPink"))
+                    .cornerRadius(5)
+                
+                HStack {
+                    Spacer()
+                    NavigationLink(destination: ProfileView(profile: modelData)) {
+                        Text("Register")
+                    }
+                    .frame(width: 100, height: 35).background(Color.black)
+                    .foregroundColor(.white)
+                    .cornerRadius(5)
+                }
+            }.padding(20)
+           
+        }.frame(
+            minWidth: 0,
+            maxWidth: .infinity,
+            minHeight: 0,
+            maxHeight: .infinity,
+            alignment: .topLeading
+        )
     }
 }
 
