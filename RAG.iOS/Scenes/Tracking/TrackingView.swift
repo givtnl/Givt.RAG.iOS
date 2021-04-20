5b48e7da64f6701a986a7947f59cad060cf9b732
Mike Pattyn <mike@givtapp.net>
2021-04-20 15:01:58 +0200

    create extra group for services and extensions

--- RAG.iOS/Scenes/Tracking/TrackingView.swift
+++ RAG.iOS/Scenes/Tracking/TrackingView.swift
@@ -8,33 +8,33 @@
 import SwiftUI
 
 struct TrackingView: View {
-    @ObservedObject var runningManager = RunningService();
+    @ObservedObject var runningService = RunningService();
     var body: some View {
         VStack {
             VStack {
-                Text(String(format: "%.1f", runningManager.currentTime))
+                Text(String(format: "%.1f", runningService.currentTime))
                     .font(Font.custom("Montserrat-Bold", size: 50))
                 HStack {
                     VStack {
-                        Text(String(format: "%.2f", runningManager.currentDistance/1000))
+                        Text(String(format: "%.2f", runningService.currentDistance/1000))
                             .font(Font.custom("Montserrat-Bold", size: 25))
                         Text("km")
                             .font(Font.custom("Montserrat-Medium", size: 12))
                     }
                     VStack {
-                        Text(String(format: "%i:%2i", runningManager.currentPace.0, runningManager.currentPace.1))
+                        Text(String(format: "%i:%2i", runningService.currentPace.0, runningService.currentPace.1))
                             .font(Font.custom("Montserrat-Bold", size: 25))
                         Text("min/km")
                             .font(Font.custom("Montserrat-Medium", size: 12))
                     }
                 }.padding(.top, 15)
             }.padding(40).background(Color("DarkerPink")).cornerRadius(10)
-            Button(runningManager.state == RunningSystemState.Running ? "Stop" : "Start") {
-                if (runningManager.state == RunningSystemState.Running) {
-                    runningManager.stopRunning()
-                    runningManager.initSystems()
+            Button(runningService.state == RunningSystemState.Running ? "Stop" : "Start") {
+                if (runningService.state == RunningSystemState.Running) {
+                    runningService.stopRunning()
+                    runningService.initSystems()
                 } else {
-                    runningManager.startRunning()
+                    runningService.startRunning()
                 }
             }
                 .font(Font.custom("Montserrat-SemiBold", size: 14))
@@ -42,9 +42,9 @@ struct TrackingView: View {
                 .foregroundColor(.white)
                 .cornerRadius(10)
                 .offset(y: 30)
-            .disabled(runningManager.state == .Initializing || runningManager.state == .Stopped)
+            .disabled(runningService.state == .Initializing || runningService.state == .Stopped)
         }.onAppear(perform: {
-            runningManager.initSystems()
+            runningService.initSystems()
         })
     }
 }
