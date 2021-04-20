6c89c1228fe0dc5f59bd8c241d69d8ac487083be
Bjorn Derudder <bjorn.derudder@gmail.com>
2021-04-20 14:46:47 +0200

    using the new view

--- RAG.iOS/Scenes/Tracking/TrackingView.swift
+++ RAG.iOS/Scenes/Tracking/TrackingView.swift
@@ -8,40 +8,49 @@
 import SwiftUI
 
 struct TrackingView: View {
-    var trackingData: TrackingViewData
-    
+    @ObservedObject var runningManager = RunningService();
     var body: some View {
         VStack {
             VStack {
-                Text(trackingData.totalTime)
+                Text(String(format: "%.1f", runningManager.currentTime))
                     .font(Font.custom("Montserrat-Bold", size: 50))
                 HStack {
                     VStack {
-                        Text(trackingData.totalDistance)
+                        Text(String(format: "%.2f", runningManager.currentDistance/1000))
                             .font(Font.custom("Montserrat-Bold", size: 25))
                         Text("km")
                             .font(Font.custom("Montserrat-Medium", size: 12))
                     }
                     VStack {
-                        Text(trackingData.totalMinutesPerUnit)
+                        Text(String(format: "%i:%2i", runningManager.currentPace.0, runningManager.currentPace.1))
                             .font(Font.custom("Montserrat-Bold", size: 25))
                         Text("min/km")
                             .font(Font.custom("Montserrat-Medium", size: 12))
                     }
                 }.padding(.top, 15)
             }.padding(40).background(Color("DarkerPink")).cornerRadius(10)
-            Button("Start") { print("Go") }
+            Button(runningManager.state == RunningSystemState.Running ? "Stop" : "Start") {
+                if (runningManager.state == RunningSystemState.Running) {
+                    runningManager.stopRunning()
+                    runningManager.initSystems()
+                } else {
+                    runningManager.startRunning()
+                }
+            }
                 .font(Font.custom("Montserrat-SemiBold", size: 14))
                 .frame(width: 100, height: 35).background(Color.black)
                 .foregroundColor(.white)
                 .cornerRadius(10)
                 .offset(y: 30)
-        }
+            .disabled(runningManager.state == .Initializing || runningManager.state == .Stopped)
+        }.onAppear(perform: {
+            runningManager.initSystems()
+        })
     }
 }
 
 struct TrackingView_Previews: PreviewProvider {
     static var previews: some View {
-        TrackingView(trackingData: TrackingViewData())
+        TrackingView()
     }
 }
