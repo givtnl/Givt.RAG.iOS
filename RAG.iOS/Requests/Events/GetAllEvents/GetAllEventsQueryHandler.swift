4576a5dc815c8b5b8c5147adaa9e29671c7539cc
Bjorn Derudder <bjorn.derudder@gmail.com>
2021-04-29 12:05:56 +0200

    temporary fix for event end date

--- RAG.iOS/Requests/Events/GetAllEvents/GetAllEventsQueryHandler.swift
+++ RAG.iOS/Requests/Events/GetAllEvents/GetAllEventsQueryHandler.swift
@@ -15,7 +15,7 @@ class GetAllEventsQueryHandler: RequestHandlerProtocol {
             var events = [Event]()
             
             response?.forEach {
-                events.append(Event(id: $0.id!, name: $0.name!, startDate: $0.startDate!, city: $0.city!, endDate: $0.endDate!))
+                events.append(Event(id: $0.id!, name: $0.name!, startDate: $0.startDate!, city: $0.city!, endDate: Date()))
             }
             
             try? completion(events as! R.TResponse)
